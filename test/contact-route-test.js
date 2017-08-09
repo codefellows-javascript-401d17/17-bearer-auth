ec6f87ccabf21aa663667756ca13b2e1f96326e4
ohjonah <ohjonah@gmail.com>
2017-08-09 08:10:15 -0700

    Get Valid Contact ID Test

--- test/contact-route-test.js
+++ test/contact-route-test.js
@@ -23,6 +23,16 @@ const exampleContact = {
 };
 
 describe('Contact Routes', function() {
+
+  afterEach( done => {
+    Promise.all([
+      User.remove({}),
+      Contact.remove({})
+    ])
+    .then( () => done())
+    .catch(done);
+  });
+
   describe('POST: /api/contact', function() {
     describe('with a valid body', function() {
       before( done => {
@@ -41,15 +51,6 @@ describe('Contact Routes', function() {
         .catch(done);
       });
 
-      after( done => {
-        Promise.all([
-          User.remove({}),
-          Contact.remove({})
-        ])
-        .then( () => done())
-        .catch(done);
-      });
-
       it('should return a contact', done => {
         request.post(`${url}/api/contact`)
         .send(exampleContact)
@@ -58,6 +59,54 @@ describe('Contact Routes', function() {
         })
         .end((err, res) => {
           if (err) return done(err);
+          expect(res.status).equal(200);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('GET: /api/contact/:id', () => {
+    describe('with a valid contact id', () => {
+      before( done => {
+        let user = new User(exampleUser);
+
+        user.generatePasswordHash(exampleUser.password)
+        .then( user => user.save())
+        .then( user => {
+          this.tempUser = user;
+          return user.generateToken();
+        })
+        .then( token => {
+          this.tempToken = token;
+          done();
+        })
+        .catch(done);
+      });
+
+      before( done => {
+        exampleContact.userID = this.tempUser._id.toString();
+        new Contact(exampleContact).save()
+        .then( contact => {
+          this.tempContact = contact;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( () => {
+        delete exampleContact.userID;
+      });
+
+      it('should return a contact', done => {
+        request.get(`${url}/api/contact/${this.tempContact._id}`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((err, res) => {
+          if (err) return done(err);
+
+          expect(res.body.name).equal(exampleContact.name);
           expect(res.status).equal(200);
           done();
         });
