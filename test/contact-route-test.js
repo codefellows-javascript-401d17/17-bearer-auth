95627cc7afe84bcae5d0e6e192c576f07911c2cd
ohjonah <ohjonah@gmail.com>
2017-08-09 08:53:48 -0700

    Adds 401 PUT Test

--- test/contact-route-test.js
+++ test/contact-route-test.js
@@ -135,52 +135,62 @@ describe('Contact Routes', function() {
   });
 
   describe('PUT: /api/contact/id:', () => {
-    describe('with a valid body', () => {
-      before( done => {
-        let user = new User(exampleUser);
+    beforeEach( done => {
+      let user = new User(exampleUser);
 
-        user.generatePasswordHash(exampleUser.password)
-        .then( user => user.save())
-        .then( user => {
-          this.tempUser = user;
-          return user.generateToken();
-        })
-        .then( token => {
-          this.tempToken = token;
-          done();
-        })
-        .catch(done);
+      user.generatePasswordHash(exampleUser.password)
+      .then( user => user.save())
+      .then( user => {
+        this.tempUser = user;
+        return user.generateToken();
+      })
+      .then( token => {
+        this.tempToken = token;
+        done();
+      })
+      .catch(done);
+    });
+
+    beforeEach( done => {
+      exampleContact.userID = this.tempUser._id.toString();
+      new Contact(exampleContact).save()
+      .then( contact => {
+        this.tempContact = contact;
+        done();
+      })
+      .catch(done);
+    });
+
+    afterEach( () => {
+      delete exampleContact.userID;
+    });
+
+    let updated = { name: 'My Updated Contact', phone: 2531111111};
+
+    it('200: should update an existing contact', done => {
+      request.put(`${url}/api/contact/${this.tempContact._id}`)
+      .send(updated)
+      .set({
+        Authorization: `Bearer ${this.tempToken}`
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).equal(200);
+        expect(res.body.name).equal(updated.name);
+        expect(res.body.phone).equal(updated.phone);
+        done();
       });
+    });
 
-      before( done => {
-        exampleContact.userID = this.tempUser._id.toString();
-        new Contact(exampleContact).save()
-        .then( contact => {
-          this.tempContact = contact;
-          done();
-        })
-        .catch(done);
-      });
-
-      after( () => {
-        delete exampleContact.userID;
-      });
-
-      it('should update an existing contact', done => {
-        let updated = { name: 'My Updated Contact', phone: 2531111111};
-
-        request.put(`${url}/api/contact/${this.tempContact._id}`)
-        .send(updated)
-        .set({
-          Authorization: `Bearer ${this.tempToken}`
-        })
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.status).equal(200);
-          expect(res.body.name).equal(updated.name);
-          expect(res.body.phone).equal(updated.phone);
-          done();
-        });
+    it('401: no token provided', done => {
+      request.put(`${url}/api/contact/${this.tempContact._id}`)
+      .send(updated)
+      .set({
+        Authorization: ''
+      })
+      .end((err, res) => {
+        expect(res.status).equal(401);
+        done();
       });
     });
   });
