04cce3d8bfebfb0f6acb9fdfe28d896fb4552b9f
ohjonah <ohjonah@gmail.com>
2017-08-09 07:55:45 -0700

    Adds Contact POST test

--- test/contact-route-test.js
+++ test/contact-route-test.js
@@ -4,6 +4,7 @@ const expect = require('chai').expect;
 const request = require('superagent');
 const User = require('../model/user.js');
 const Contact = require('../model/contact.js');
+const Promise = require('bluebird');
 
 require('../server.js');
 
@@ -22,7 +23,45 @@ const exampleContact = {
 };
 
 describe('Contact Routes', function() {
-  describe('POST: /api/signup', function() {
-    describe('with a valid body')
+  describe('POST: /api/contact', function() {
+    describe('with a valid body', function() {
+      before( done => {
+        let user = new User(exampleUser);
+
+        user.generatePasswordHash(exampleUser.password)
+        .then( user => user.save())
+        .then( user => {
+          this.tempUser = user;
+          return user.generateToken();
+        })
+        .then( token => {
+          this.tempToken = token;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( done => {
+        Promise.all([
+          User.remove({}),
+          Contact.remove({})
+        ])
+        .then( () => done())
+        .catch(done);
+      });
+
+      it('should return a contact', done => {
+        request.post(`${url}/api/contact`)
+        .send(exampleContact)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).equal(200);
+          done();
+        });
+      });
+    });
   });
 });
