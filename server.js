44b8aea588eab2ebf519dff6488ffc0fea9fdcaa
Allan Liebold <allan.liebold@gmail.com>
2017-08-09 02:40:24 -0700

    adds bearer-auth-middleware

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const debug = require('debug')('atlas:server');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const authRouter = require('./route/auth-router.js');
+const regionRouter = require('./route/region-router.js');
 const errors = require('./lib/error-middleware.js');
 
 dotenv.load();
@@ -17,7 +19,9 @@ mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
 app.use(morgan('dev'));
+
 app.use(authRouter);
+app.use(regionRouter);
 app.use(errors);
 
 app.listen(PORT, () => {
